09fda3d1e78f1df4832bab1188dae15f9dfc4e98
h.drelon <helene.drelon@hotmail.fr>
2023-02-21 09:43:38 +0100

    MAJ animal repository

--- src/Repository/AnimalRepository.php
+++ src/Repository/AnimalRepository.php
@@ -78,18 +78,5 @@ public function articleParSonIdUtilisateur($value): array
         ->getResult()
     ;
 }
-public function imagesParSonIdAnimal($value): array
-{
-    return $this->createQueryBuilder('a')
-        ->andWhere('a.idAnimal = :val')
-        ->setParameter('val', $value)
-        ->Join(Image::class, 'i', 'WITH', 'a.idAnimal = i.idAnimal')
-        ->addSelect('i.image')
-        ->orderBy('i.idAnimal')
-        ->setMaxResults(1)
-        ->getQuery()
-        ->getResult()
-    ;
-}
 
 }
